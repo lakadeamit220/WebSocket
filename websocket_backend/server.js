d32e074e700fe944d78400e8f5873881a1c9c178
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-03-04 12:33:32 +0530

    implemented backend server of the application

--- websocket_backend/server.js
+++ websocket_backend/server.js
@@ -1,1 +1,36 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('WebSocket server is running');
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('message', (data) => {
+    console.log(`Message received: ${data}`);
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
